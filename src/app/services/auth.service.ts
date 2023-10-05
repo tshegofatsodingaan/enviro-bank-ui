c3dc6102fbd56965b8f0998e2adc90712ce65bd7
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-10-05 15:04:05 +0200

    added a new endpoint for the resetting of the password to be injected in the component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,4 +14,10 @@ export class AuthService {
       headers: new HttpHeaders().set('Content-Type', 'application/json')
     });
   }
+
+  public resetPassword(user: any): Observable<any>{
+    return this.http.post<any>('/api/v1/auth/reset-password', JSON.stringify(user),{
+      headers: new HttpHeaders().set('Content-Type', 'applications/json')
+    });
+  }
 }
