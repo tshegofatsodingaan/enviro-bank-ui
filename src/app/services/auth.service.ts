86315a534cb89c0d8cf75d439ec031de51f2cdd8
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-10-05 09:49:09 +0200

    new material folder that contains a module for angular material to be imported in the app module

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // @ts-ignore
+  private signIn(credentials: any): Observable<any>{
+    return this.http.post<any>("/api/v1/auth/sign-in", JSON.stringify(credentials), {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    });
+  }
 }
