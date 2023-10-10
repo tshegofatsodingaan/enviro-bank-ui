fb23820f119c13a40cace392542dbe239e318df6
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-10-10 16:16:27 +0200

    added an enum class for type of accounts

---
+++ src/app/models/account.model.ts
@@ -0,0 +1,11 @@
+import {AccountType} from "../enums/account-type.enum";
+
+export class Account {
+  id = 0;
+  accountNum = 0;
+  customerNum = '';
+  accountType: AccountType = AccountType.WITHDRAW;
+  accountBalance = 0;
+  active = true;
+  customer = 0;
+}
