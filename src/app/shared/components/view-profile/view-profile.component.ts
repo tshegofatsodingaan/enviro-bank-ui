e2387d80fa01c59622af15fe31bab1d59b10739c
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-10-19 21:08:37 +0200

    Added a view accounts component

--- src/app/shared/components/view-profile/view-profile.component.ts
+++ src/app/shared/components/view-profile/view-profile.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+import {Customer} from "../../../models/customer.model";
+import {Account} from "../../../models/account.model";
+
 
 @Component({
   selector: 'app-view-profile',
   templateUrl: './view-profile.component.html',
   styleUrls: ['./view-profile.component.css']
 })
-export class ViewProfileComponent {
+export class ViewProfileComponent implements OnInit{
 
+  user: Customer | undefined;
+  accounts: Account[] = [];
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private route: Router,
+              private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    if(id){
+      this.authService.getUser(id).subscribe(data => {
+        this.user = data;
+      });
+      this.gatherUserAccounts();
+    }
+
+  }
+
+  public gatherUserAccounts(){
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    if(id){
+      this.authService.getAllAccounts(id, {size: 3, page: 0}).subscribe(data => {
+        this.accounts = data;
+        console.log(this.accounts);
+      })
+    }
+
+
+  }
+
+  public generateInitials(): any {
+    if(this.user){
+      const nameInitial = this.user.name.charAt(0).toUpperCase();
+      const surnameInitial = this.user.surname.charAt(0).toUpperCase();
+      return nameInitial + surnameInitial;
+    }
+  }
+
+  transferFunds() {
+    this.route.navigateByUrl('customer/transfer-funds');
+  }
 }
