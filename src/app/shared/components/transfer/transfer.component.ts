7af4562f409b60c7277474094858d03f88cbac19
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-10-26 14:10:10 +0200

    added a dialog popup on the transfer form

--- src/app/shared/components/transfer/transfer.component.ts
+++ src/app/shared/components/transfer/transfer.component.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
-import {CustomerService} from "../../services/customer.service";
-import {AuthService} from "../../../shared/services/auth.service";
+import {CustomerService} from "../../../customer/services/customer.service";
+import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {DialogBoxComponent} from "../dialog-box/dialog-box.component";
 
 @Component({
   selector: 'app-transfer',
@@ -11,14 +13,15 @@ import {MatSnackBar} from "@angular/material/snack-bar";
   styleUrls: ['./transfer.component.css']
 })
 export class TransferComponent{
-  snackBarMessage = 'Funds transferred successfully!';
-  durationInSeconds = 2;
+
+
 
   constructor(private formBuilder: FormBuilder,
               private customerService: CustomerService,
               private authService: AuthService,
               private route: Router,
-              private snackBar: MatSnackBar) {
+              private snackBar: MatSnackBar,
+              private dialog: MatDialog) {
   }
 
 
@@ -29,16 +32,25 @@ export class TransferComponent{
     transactionAmount: ['', [Validators.required]]
   })
 
+  dialogPopUp(){
+    const mdConfig = new MatDialogConfig();
+    mdConfig.width = '400px';
+    mdConfig.data = {
+      title: 'Confirm',
+      content: 'Are you sure you want to proceed with this transaction?'
+    }
+    const dialogReference = this.dialog.open(DialogBoxComponent, mdConfig);
+    dialogReference.afterClosed().subscribe(result => {
+      this.transferFundsFormGroup.reset();
+    })
+  }
+
   transfer(){
     if(this.transferFundsFormGroup.valid){
       const enviro_bank_session = this.authService.session;
       this.customerService.transferFunds(enviro_bank_session.token, this.transferFundsFormGroup.value as string).subscribe(data => {
-      this.snackBar.open(this.snackBarMessage, 'Close', {
-        duration: this.durationInSeconds * 1000,
-        verticalPosition: 'top',
-        horizontalPosition: 'right',
-        panelClass: 'success-snackbar'
-      })
+        this.dialogPopUp();
+
       });
     }
     return
