ecad2720f5efb4907d61458a0520a21fefcfc239
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-10-26 14:09:49 +0200

    added a new dynamic dialog box

---
+++ src/app/shared/components/dialog-box/dialog-box.component.ts
@@ -0,0 +1,35 @@
+import {Component, Inject} from '@angular/core';
+import {Router} from "@angular/router";
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
+@Component({
+  selector: 'app-dialog-box',
+  templateUrl: './dialog-box.component.html',
+  styleUrls: ['./dialog-box.component.css']
+})
+export class DialogBoxComponent {
+
+  public dialogDetails: { title?: string, content?: string } = {};
+  snackBarMessage = 'Funds transferred successfully!';
+  durationInSeconds = 2;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+              private router: Router,
+              private snackBar: MatSnackBar) {
+    this.dialogDetails = data;
+  }
+
+  onClickCancel() {
+    this.router.navigateByUrl('/shared/transfer-funds');
+  }
+
+  onClickContinue() {
+    this.snackBar.open(this.snackBarMessage, 'Close', {
+      duration: this.durationInSeconds * 1000,
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+      panelClass: 'success-snackbar'
+    })
+  }
+
+}
