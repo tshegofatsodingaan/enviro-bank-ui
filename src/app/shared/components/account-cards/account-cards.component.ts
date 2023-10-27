f56c39c6ee0fa645e1bdd11a668d13ede1b79f05
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-10-27 08:17:50 +0200

    added an account card component to be injected in certain components

---
+++ src/app/shared/components/account-cards/account-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-cards',
+  templateUrl: './account-cards.component.html',
+  styleUrls: ['./account-cards.component.css']
+})
+export class AccountCardsComponent {
+
+}
