bd088a6b1ef11bc6155d33468a15809c5e4235f2
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-11-02 19:51:02 +0200

    updated a completed version of the change password page for when the user is not yet logged in

--- src/app/shared/components/change-password/change-password.component.spec.ts
+++ src/app/shared/components/change-password/change-password.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangePasswordComponent } from './change-password.component';
 
-describe('ChangePasswordComponent', () => {
+describe('ChangePasswordBeforeLoginComponent', () => {
   let component: ChangePasswordComponent;
   let fixture: ComponentFixture<ChangePasswordComponent>;
 
--- src/app/shared/components/change-password/change-password.component.ts
+++ src/app/shared/components/change-password/change-password.component.ts
@@ -1,20 +1,20 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute, Router} from "@angular/router";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../../services/auth.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-change-password',
+  selector: 'app-change-password-before-login',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit{
 
   changePasswordFormGroup: FormGroup = new FormGroup({});
-  uniquePassword: boolean = false;
   durationInSeconds = 2;
   snackBarMessage = 'Password updated successfully!'
+  invalidCredentials: boolean = false;
 
   constructor(private route: Router, private formBuilder: FormBuilder,
               private authService: AuthService,
@@ -44,15 +44,14 @@ export class ChangePasswordComponent implements OnInit{
         newPassword: this.changePasswordFormGroup.get('newPassword')?.value as string,
         confirmPassword: this.changePasswordFormGroup.get('confirmPassword')?.value as string
       }
-
-      const tokenSession = this.authService.session;
-      this.authService.changePassword(passwords, tokenSession.token).subscribe((error) => {
+      const userToken = this.activatedRoute.snapshot.queryParams['token'];
+      this.authService.changePasswordBeforeLogin(passwords, userToken).subscribe((error) => {
         this.displaySnackBar();
+        this.route.navigateByUrl('');
         if (error.status === 403){
-          this.uniquePassword = true
+          this.invalidCredentials = true;
         }
       })
     }
   }
-
 }
